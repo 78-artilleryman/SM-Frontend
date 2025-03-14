b951e9a6b515c74bd755f4e1fc483fb665cf127c
78-artilleryman <ywy040150@gmail.com>
2025-03-14 07:51:11 +0000

    [design] : 스타일 변경

--- src/feature/my/ui/UserContent.tsx
+++ src/feature/my/ui/UserContent.tsx
@@ -4,8 +4,9 @@ import InputWithButton from "@/shared/ui/InputWithButton";
 import InputWithLabel from "@/shared/ui/InputWithLabel";
 import { validatePassword } from "@/shared/utils";
 import { ChangeEvent, Dispatch, FormEvent, MouseEvent, SetStateAction, useEffect, useState } from "react";
-import { myPasswordChangeRequest, userInformationRequest } from "../service";
+import { myPasswordChangeRequest, passwordCheckRequest, userInformationRequest } from "../service";
 import { useAuth } from "@/app/providers/AuthProvider";
+import { useToast } from "@/app/providers/ToastProvider";
 
 interface UserContentProps {
   setIsModalState: Dispatch<SetStateAction<boolean>>;
@@ -21,12 +22,14 @@ function UserContent({ setIsModalState }: UserContentProps) {
   });
 
   const [errors, setErrors] = useState<Errors>({
+    password: "",
     newPassword: "",
     rePassword: "",
   });
   const [isOpen, setOpen] = useState(false);
 
   const { getUser } = useAuth();
+  const { addToast } = useToast();
   const token = getUser();
 
   useEffect(() => {
@@ -65,9 +68,34 @@ function UserContent({ setIsModalState }: UserContentProps) {
   };
 
   // 현재 비밀번호 입력 후 변경하기 버튼 클릭 시 새 비밀번호 인풋을 열어줌
-  const handleInputOpen = (event: MouseEvent<HTMLButtonElement>) => {
+  const handleInputOpen = async (event: MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
-    setOpen(true);
+
+    try {
+      const token = getUser();
+      const response = await passwordCheckRequest(token, formData.password);
+
+      if (response.success) {
+        setOpen(true);
+        setErrors(prevErrors => ({
+          ...prevErrors,
+          password: "",
+        }));
+      }
+    } catch (err: any) {
+      console.error(err);
+      if (err.message.includes("401")) {
+        setErrors(prevErrors => ({
+          ...prevErrors,
+          password: "비밀번호가 틀렸습니다.",
+        }));
+      } else {
+        setErrors(prevErrors => ({
+          ...prevErrors,
+          password: "비밀번호 확인 중 오류가 발생했습니다.",
+        }));
+      }
+    }
   };
 
   // 화면에 보이는 인풋 필드들이 모두 채워졌는지 확인하는 조건
@@ -99,15 +127,17 @@ function UserContent({ setIsModalState }: UserContentProps) {
 
       if (response.success) {
         setIsModalState(false);
+        addToast("비밀번호가 변경되었습니다.", "success");
       }
     } catch (error: any) {
       console.error("비밀번호 변경 에러 발생");
+      addToast("비밀번호에 실패하였습니다.", "error");
     }
   };
 
   return (
     <div className="flex flex-col w-full items-center px-[90px] pt-[80px] pb-[100px]">
-      <h3 className="w-full text-gray-800 text-title-lg mb-14">유저정보</h3>
+      <h3 className="w-full text-gray-800 text-title-lg mb-10">유저정보</h3>
       <form className="w-full flex flex-col gap-5" onSubmit={handleSubmit}>
         <InputWithLabel
           label="성함"
@@ -137,6 +167,7 @@ function UserContent({ setIsModalState }: UserContentProps) {
           disabled={formData.password === ""}
           buttonText="변경하기"
           onClick={handleInputOpen}
+          error={errors.password}
         />
         {isOpen && (
           <InputWithLabel
